4a20c42371bd9393836b52a8c12787ef8617f796
Sy <2548446071@qq.com>
2025-02-17 12:16:28 +0800

    feat: 主页

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,55 @@
+"use server";
 import "./index.css";
-import {listQuestionBankVoByPageUsingPost} from "@/api/questionBankController";
+import { listQuestionBankVoByPageUsingPost } from "@/api/questionBankController";
+import {Divider, Flex, message} from "antd";
+import {listQuestionVoByPageUsingPost} from "@/api/questionController";
+import Link from "next/link";
+import QuestionBankList from "@/components/QuestionBankList";
+import QuestionList from "@/components/QuestionList";
+import Title from "antd/es/typography/Title";
 
 /**
  * 主页
  * @constructor
  */
-export default function HomePage() {
-  listQuestionBankVoByPageUsingPost({}).then((res) => {
-    console.log(res);
-  });
+export default async function HomePage() {
+  let questionBankList = [];
+  let questionList = [];
+  try {
+    const res = await listQuestionBankVoByPageUsingPost({
+      pageSize: 12,
+      sortField: "createTime",
+      sortOrder: "descend",
+    });
+    questionBankList = res.data.records ?? [];
+  } catch (e) {
+    message.error("获取题库列表失败，" + e.message);
+  }
 
-  return <div id="homePage">主页</div>;
+  try {
+    const res = await listQuestionVoByPageUsingPost({
+      pageSize: 12,
+      sortField: "createTime",
+      sortOrder: "descend",
+    });
+    questionList = res.data.records ?? [];
+  } catch (e) {
+    message.error("获取题目列表失败，" + e.message);
+  }
+
+  return (
+    <div id="homePage" className="max-width-content">
+      <Flex justify="space-between" align="center">
+        <Title level={3}>最新题库</Title>
+        <Link href={"/banks"}>查看更多</Link>
+      </Flex>
+      <QuestionBankList questionBankList={questionBankList} />
+      <Divider />
+      <Flex justify="space-between" align="center">
+        <Title level={3}>最新题目</Title>
+        <Link href={"/questions"}>查看更多</Link>
+      </Flex>
+      <QuestionList questionList={questionList} />
+    </div>
+  );
 }
